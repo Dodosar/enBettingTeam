363df30c4fb89dd0e91408c146453590499a72a8
Dodosar <depechemode1989@mail.ru>
2018-07-13 12:32:58 +0300

    Add Email Report Sharing

--- betSite/src/test/java/betSite/WebDriverSettings.java
+++ betSite/src/test/java/betSite/WebDriverSettings.java
@@ -7,7 +7,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+
+
+
+
+import javax.mail.internet.MimeMultipart;
+
+
+
+
+
+
 
 
 
@@ -23,6 +67,7 @@ import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 //import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
@@ -175,6 +220,78 @@ public class WebDriverSettings {
 			driver.quit();	
 		}
 	}
+	
+	@AfterSuite
+	public void tearDown(){
+		sendPDFReportByGMail("tyudm@anzer.com", "Lbvf650065", "tiurindmitry1989@gmail.com", "PDF Report", "Test how will sent PDF file by email");
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+	
+	/**
+	 * Send email using java
+	 * @param from
+	 * @param pass
+	 * @param to
+	 * @param subject
+	 * @param body
+	 */
+	private static void sendPDFReportByGMail(String from, String pass, String to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+        	//Set from address
+            message.setFrom(new InternetAddress(from));
+             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+           //Set subject
+            message.setSubject(subject);
+            message.setText(body);
+          
+            BodyPart objMessageBodyPart = new MimeBodyPart();
+            
+            objMessageBodyPart.setText("Please Find The Attached Report File!");
+            
+            Multipart multipart = new MimeMultipart();
+
+            multipart.addBodyPart(objMessageBodyPart);
+
+            objMessageBodyPart = new MimeBodyPart();
+
+            //Set path to the pdf report file
+            String filename = System.getProperty("user.dir")+"\\Default test.pdf"; 
+            //Create data source to attach the file in mail
+            DataSource source = new FileDataSource(filename);
+            
+            objMessageBodyPart.setDataHandler(new DataHandler(source));
+
+            objMessageBodyPart.setFileName(filename);
+
+            multipart.addBodyPart(objMessageBodyPart);
+
+            message.setContent(multipart);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
 
 }
 
