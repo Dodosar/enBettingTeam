d2dc1c40edda4d559be96863fc432c79d1437086
Dodosar <depechemode1989@mail.ru>
2019-02-22 19:11:25 +0200

    selenide

--- betSite/src/test/java/betSite/PutInBet.java
+++ betSite/src/test/java/betSite/PutInBet.java
@@ -1,7 +1,10 @@
 package betSite;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 /*
  *created by Dima Tiurin Sep 26, 2018
@@ -18,6 +21,12 @@ public class PutInBet extends WebDriverSettings{
 		System.out.println("Step 1: Check Title on Matches Page");
 		objBettingSite
 					.mainPage().open().clickOn("matchesPage");
+	}
+	
+	@Test(priority = 1)
+	private void writeMesssage() {
+		$(By.xpath("//*[@class='bi-field w-chat-send__field bi-mod-input-placeholder "
+				+ "bi-field--theme-gray']")).setValue("TestDima").pressEnter();
 	}
 }
 
